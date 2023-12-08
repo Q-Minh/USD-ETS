92bbee7ae871e7eebc40e14ecb07df3aa5394886
Q-Minh <tonthat.quocminh@gmail.com>
2023-12-07 21:08:12 -0500

    Add help messages for CLI arguments

--- main.py
+++ main.py
@@ -10,16 +10,43 @@ if __name__ == "__main__":
         prog="USD writer for the ETS multimedia research lab",
         description="""Creates animations in the USD file format.""",
     )
-    parser.add_argument("-i", "--input", default=".", type=str, dest="input")
-    parser.add_argument("-o", "--output", default="out.usd", type=str, dest="output")
-    parser.add_argument("--fps", default=60, type=int, dest="fps")
-    parser.add_argument("--up-axis", default="Z", type=str, dest="up_axis")
+    parser.add_argument(
+        "-i", "--input", 
+        default=".", 
+        type=str, 
+        dest="input", 
+        help="Input path containing simulation data to render to USD.")
+    parser.add_argument(
+        "-o", "--output", 
+        default="out.usd", 
+        type=str, 
+        dest="output",
+        help="Output USD file (must end with .usd | .usda | .usdc)")
+    parser.add_argument(
+        "--fps", 
+        default=60, 
+        type=int, 
+        dest="fps", 
+        help="Frames per second with which the simulation data input was generated.")
+    parser.add_argument(
+        "--up-axis", 
+        default="Z", 
+        type=str, 
+        dest="up_axis",
+        help="The up axis, one of ('Y' or 'Z' or less probably 'X')")
     parser.add_argument(
         "--from-pba",
         action="store_true",
-        dest="from_pba"
+        dest="from_pba",
+        help="Set to true if the simulation data input comes from the PBA toolkit."
     )
-    parser.add_argument("--lazy", action="store_true", dest="lazy")
+    parser.add_argument(
+        "--lazy", 
+        action="store_true", 
+        dest="lazy",
+        help="Whether to load the input simulation data all at once before USD " + 
+            "rendering, or one at a time during USD rendering.")
+    
     args = parser.parse_args()
     stage = Usd.Stage.CreateNew(args.output)
     renderer = UsdRenderer(stage, up_axis=args.up_axis, fps=args.fps)
